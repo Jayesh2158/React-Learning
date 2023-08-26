7cf836b3085b63e9d5b4ba419eed676546a1f5e4
Jayesh2158 <81233576+Jayesh2158@users.noreply.github.com>
2023-08-27 00:40:21 +0530

    add/createBrowserRouter, RouterProvider, Outlet (#6)

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <h2>Somthing will be here soon.</h2>
+        </div>
+    )
+}
+
+export default About;

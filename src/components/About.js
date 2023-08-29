8611077ea0ae2635a9df8dfc926756a80671c2f0
Jayesh2158 <jayeshchoudhary939570@gmail.com>
2023-08-29 23:11:57 +0530

    add/class base component, componentDidMount

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import UserClass from "./UserClass"
+
+
 const About = () => {
     return (
         <div>
             <h1>About Us</h1>
             <h2>Somthing will be here soon.</h2>
+            <UserClass />
         </div>
     )
 }
